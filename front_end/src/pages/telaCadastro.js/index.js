4b62fd004533898cdbd3abc635623e1844333f6d
MarkusLima <47782867+MarkusLima@users.noreply.github.com>
2020-07-14 20:10:27 -0300

    14/07/2020
    Co-Authored-By: markuslima <47782867+markuslima@users.noreply.github.com>

--- front_end/src/pages/telaCadastro.js/index.js
+++ front_end/src/pages/telaCadastro.js/index.js
@@ -1,18 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Button, Form } from 'react-bootstrap';
+import { Card, Button, Form, Row, Col } from 'react-bootstrap';
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from "react-places-autocomplete";
 
 export default function TelaCadastro() {
     const [dt, setDt] = useState(null);
-    const [address, setAddress] = React.useState(null);
+    const [enderecoPartida, setEnderecoPartida] = React.useState("");
+    const [enderecoDestino, setEnderecoDestino] = React.useState("");
     const [coordinates, setCoordinates] = React.useState({ lat: null, lng: null });
+    const [coordinatesDest, setCoordinatesDestino] = React.useState({ lat: null, lng: null });
+
+
+    const [nome, setNome] = useState('');
+    const [dateEntrega, setDateEntrega] = useState('');
+
 
     const handleSelect = async value => {
         const results = await geocodeByAddress(value);
         const latLng = await getLatLng(results[0]);
-        setAddress(value);
+        setEnderecoPartida(value);
         setCoordinates(latLng);
     };
+
+    const handleSelectDestino = async value => {
+        const resultsDest = await geocodeByAddress(value);
+        const latLng = await getLatLng(resultsDest[0]);
+        setEnderecoDestino(value);
+        setCoordinatesDestino(latLng);
+    };
+
 
     useEffect(() => {
         var dt = new Date();
@@ -31,51 +46,64 @@ export default function TelaCadastro() {
 
     return (
 
-        <div className="container fluid">
+        <div className="container">
             <br />
-            <Card className="text-center" bg="light">
-                <Card.Header>Cadastro de Entregas</Card.Header>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Control type="text" placeholder="Digite um nome..." required />
-                    </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Control type="date" placeholder="Digite a data de entrega..." required />
-                    </Form.Group>
+            <Row className="row justify-content-md-center">
+                <Col className="col col-lg-5">
+                    <Card className="text-center" bg="light">
+                        <Card.Header>Cadastro de Entregas</Card.Header>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Control type="text" placeholder="Digite um nome..." required value={nome} onChange={event => setNome(event.target.value)} />
+                            </Form.Group>
 
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Control type="text" placeholder="Digite local de partida..." required />
-                    </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Control type="date" placeholder="Digite a data de entrega..." required value={dateEntrega} onChange={event => setDateEntrega(event.target.value)} />
+                            </Form.Group>
 
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Control type="text" placeholder="Digite o destino..." required />
-                    </Form.Group>
+                            <PlacesAutocomplete value={enderecoPartida} onChange={setEnderecoPartida} onSelect={handleSelect}>
+                                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                                    <div> 
+                                        {/* <p>Latitude: {coordinates.lat}</p><p>Longitude: {coordinates.lng}</p><p>Adress: {enderecoPartida}</p> */}
+                                        <Form.Group controlId="formBasicEmail">
+                                            <Form.Control type="text" required {...getInputProps({ placeholder: "Digite local de partida..." })}/>
+                                        </Form.Group>
+                                        <div>
+                                            {loading ? <div>Loading...</div> : null}
+                                            {suggestions.map(suggestion => {return (<div {...getSuggestionItemProps(suggestion)}>{suggestion.description}</div>); })}
+                                        </div>
+                                    </div>
+                                )}
+                            </PlacesAutocomplete>
 
-                    <Button variant="primary" type="buttont">Cadastrar</Button>
-                </Form>
-                <Card.Footer className="text-muted">{dt}</Card.Footer>
-            </Card>
+                            <PlacesAutocomplete value={enderecoDestino} onChange={setEnderecoDestino} onSelect={handleSelectDestino}>
+                                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                                    <div> 
+                                        {/* <p>Latitude: {coordinatesDest.lat}</p><p>Longitude: {coordinatesDest.lng}</p><p>Adress: {enderecoDestino}</p> */}
+                                        <Form.Group controlId="formBasicEmail">
+                                            <Form.Control type="text" required {...getInputProps({ placeholder: "Digite local de Destino..." })}/>
+                                        </Form.Group>
+                                        <div>
+                                            {loading ? <div>Loading...</div> : null}
+                                            {suggestions.map(suggestion => {return (<div {...getSuggestionItemProps(suggestion)}>{suggestion.description}</div>); })}
+                                        </div>
+                                    </div>
+                                )}
+                            </PlacesAutocomplete>
+                          
 
-            <PlacesAutocomplete value={address} onChange={setAddress} googleCallbackName={handleSelect}>
-                {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-                    <div>
-                        <p>Latitude: {coordinates.lat}</p>
-                        <p>Longitude: {coordinates.lng}</p>
+                            <Button variant="primary" type="button">Cadastrar</Button>
+                        </Form>
+                        <Card.Footer className="text-muted">{dt}</Card.Footer>
+                    </Card>
+                </Col>
+                <Col className="col col-lg-7">
+                 
+                </Col>
+            </Row>
 
-                        <input {...getInputProps({ placeholder: "Type address" })} />
-
-                        <div>
-                            {loading ? <div>Loading...</div> : null}
-
-                            {suggestions.map(suggestion => {
-                                const style = { backgroundColor: suggestion.active ? "#41b6e6" : "#fff" };
-                                return (<div {...getSuggestionItemProps(suggestion, { style })}>{suggestion.description}</div>);
-                            })}
-                        </div>
-                    </div>
-                )}
-            </PlacesAutocomplete>
         </div>
     )
 }
+
